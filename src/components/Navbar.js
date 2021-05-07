c400c207a2a0c5e89b31b836d292ea8f11844265
unknown <karolis.soblinskas@gmail.com>
2021-05-07 17:18:12 +0300

    navbar update'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar({favoriteArr}) {
              <Link className="link" to="/">Home</Link>
          </div>
          <div>
-             <Link className="link" to="/allrecipes">All Recipes</Link>
+             <Link className="link" to="/allrecipes">Recipes</Link>
              <Link className="link" to="/favorite">Favorite Recipes ({favoriteArr.length})</Link>
              <Link className="link" to="/search">Search</Link>
              <Link className="link" to="/upload">Upload Recipe</Link>
